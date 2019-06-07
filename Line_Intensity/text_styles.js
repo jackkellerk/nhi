232291756c59663e166534feccd106ac2e687d7d
TJBarnes220 <46406789+TJBarnes220@users.noreply.github.com>
2019-06-07 14:00:08 -0400

    Added Line intensity

---
+++ Line_Intensity/text_styles.js
@@ -0,0 +1,24 @@
+const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    align: 'center',
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 500,
+});
+
+//Text style for words appearing on graph
+const graphText = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 7,
+    align: 'center',
+});

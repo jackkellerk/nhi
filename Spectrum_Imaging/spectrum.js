b72aaee1516f01838276f27c03b5bb69c32ae80c
desaixie <xiedesaigg@gmail.com>
2019-06-07 15:03:21 -0400

    Using class Spectrum. Made all functions static to avoid naming conflict

--- Spectrum_Imaging/spectrum.js
+++ Spectrum_Imaging/spectrum.js
@@ -4,132 +4,97 @@ todo:
     draw box and tint only inside the box
  */
 
-//globals
-// let colors = ['white', 'black', 'yellow', 'red', 'green', 'orange', 'blue', 'grey', 'purple'];
 let dropdownContainer = new PIXI.Container();
 let optionContainer = new PIXI.Container();
 let metalsprite, dropdownBtn, optionBtns;
 
-function pixiSetup() {
-//     let type = "WebGL";
-//     if (!PIXI.utils.isWebGLSupported()) {
-//         type = "canvas";
-//     }
-//     PIXI.utils.sayHello(type);
-//
-// //Create a PixiJS Application
-//     app = new PIXI.Application();
-//     app = new PIXI.Application({
-//             width: 800,         // default: 800
-//             height: window.innerHeight,        // default: 600
-//             antialias: true,    // default: false
-//             transparent: false, // default: false
-//             resolution: 1,       // default: 1
-//         }
-//     );
+class Spectrum{
+//globals
+// let colors = ['white', 'black', 'yellow', 'red', 'green', 'orange', 'blue', 'grey', 'purple'];
 
-// Set canvas to fill the entire window. Use with "<style>* {padding: 0; margin: 0}</style>" in html
-//     app.renderer.view.style.position = "absolute";
-//     app.renderer.view.style.display = "block";
-//     app.renderer.autoDensity = true;
-// app.renderer.resize(container.innerWidth, container.innerHeight);
-
-//Add the canvas that Pixi automatically created for you to the HTML document
-//     document.body.appendChild(app.view);
-    metalsprite = new PIXI.Sprite.from('Images/sinteredMetal.png');
-    metalsprite.anchor.set(0.5);
-    metalsprite.scale.set(2);
-    app.stage.addChild(metalsprite);
-}
-
-
-function getHexColor(colorStr) {
-    var a = document.createElement('div');
-    a.style.color = colorStr;
-    var colors = window.getComputedStyle( document.body.appendChild(a) ).color.match(/\d+/g).map(function(a){ return parseInt(a,10); });
-    document.body.removeChild(a);
-    return (colors.length >= 3) ? '0x' + (((1 << 24) + (colors[0] << 16) + (colors[1] << 8) + colors[2]).toString(16).substr(1)) : false;
-}
-
-function onClickDropdownBtn(){
-    optionContainer.visible = !optionContainer.visible;
-}
-
-
-
-function onClickColorOptions(color){
-    switch(color) {
-        case "red":
-            metalsprite.tint = getHexColor("red");
-            break;
-        case "green":
-            metalsprite.tint = getHexColor("green");
-            break;
-        case "blue":
-            metalsprite.tint = getHexColor("blue");
-            break;
-        case "original":
-            metalsprite.tint = 0xFFFFFF;
-            break;
-        default: void(0);
+    static pixiSetup() {
+        metalsprite = new PIXI.Sprite.from('Images/sinteredMetal.png');
+        metalsprite.anchor.set(0.5);
+        metalsprite.scale.set(2);
+        app.stage.addChild(metalsprite);
     }
-}
 
-function createColorPickDropdown(){
-    dropdownBtn = createButton(20,app.screen.height-80, 60, 60,
-            onClickDropdownBtn);
-    dropdownBtn.buttonMode = true;
-    dropdownContainer.addChild(dropdownBtn);
-    
-    optionBtns = [];
-    let options = ["red", "green", "blue", "original"];
-    let texts = [];
-    for (let i = 0; i < 4; i++) {
-        optionBtns.push(createButton(85 + i*65, app.screen.height-80, 60, 60,
-                function(){onClickColorOptions(options[i])}));
-        optionBtns[i].buttonMode = true;
-        texts.push(new PIXI.Text(options[i]));
-        texts[i].style = {align: "center"};
-        optionBtns[i].addChild(texts[i]);
-        optionContainer.addChild(optionBtns[i]);
+
+    static getHexColor(colorStr) {
+        let a = document.createElement('div');
+        a.style.color = colorStr;
+        let colors = window.getComputedStyle( document.body.appendChild(a) ).color.match(/\d+/g).map(function(a){ return parseInt(a,10); });
+        document.body.removeChild(a);
+        return (colors.length >= 3) ? '0x' + (((1 << 24) + (colors[0] << 16) + (colors[1] << 8) + colors[2]).toString(16).substr(1)) : false;
     }
-    optionContainer.visible = false;
-    dropdownContainer.addChild(optionContainer);
-    
-    app.stage.addChild(dropdownContainer);
-}
+
+    static onClickDropdownBtn(){
+        optionContainer.visible = !optionContainer.visible;
+    }
+
+    static onClickColorOptions(color){
+        switch(color) {
+            case "red":
+                metalsprite.tint = Spectrum.getHexColor("red");
+                break;
+            case "green":
+                metalsprite.tint = Spectrum.getHexColor("green");
+                break;
+            case "blue":
+                metalsprite.tint = Spectrum.getHexColor("blue");
+                break;
+            case "original":
+                metalsprite.tint = 0xFFFFFF;
+                break;
+            default: void(0);
+        }
+    }
+
+    static createColorPickDropdown(){
+        dropdownBtn = Spectrum.createButton(20,app.screen.height-80, 60, 60,
+            Spectrum.onClickDropdownBtn);
+        dropdownBtn.buttonMode = true;
+        dropdownContainer.addChild(dropdownBtn);
+
+        optionBtns = [];
+        let options = ["red", "green", "blue", "original"];
+        let texts = [];
+        for (let i = 0; i < 4; i++) {
+            optionBtns.push(Spectrum.createButton(85 + i*65, app.screen.height-80, 60, 60,
+                function(){Spectrum.onClickColorOptions(options[i])}));
+            optionBtns[i].buttonMode = true;
+            texts.push(new PIXI.Text(options[i]));
+            texts[i].style = {align: "center"};
+            optionBtns[i].addChild(texts[i]);
+            optionContainer.addChild(optionBtns[i]);
+        }
+        optionContainer.visible = false;
+        dropdownContainer.addChild(optionContainer);
+
+        app.stage.addChild(dropdownContainer);
+    }
 
 // actions: [pointerdown, pointerup, mouseover, mouseout]
-function createButton(x_position, y_position, height, width, clickAction)
-{
-    var graphics = new PIXI.Graphics();
-    graphics.beginFill(0xFFFFFF, 1);
-    graphics.lineStyle(2, 0x414141, 1);
-    graphics.drawRect(x_position, y_position, width, height);
-    graphics.endFill();
-    graphics.interactive = true;
-    graphics.on('mouseover', onHoverOver)
-            .on('mouseout', onHoverOff);
-    if(clickAction){
-        graphics.on('pointerdown', clickAction)
+    static createButton(x, y, height, width, clickAction)
+    {
+        let graphics = new PIXI.Graphics();
+        graphics.beginFill(0xFFFFFF, 1);
+        graphics.lineStyle(2, 0x414141, 1);
+        graphics.drawRect(x, y, width, height);
+        graphics.endFill();
+        graphics.interactive = true;
+        graphics.on('mouseover', function(){ this.alpha = 0.5; })
+            .on('mouseout', function(){ this.alpha = 1; });
+        if(clickAction){
+            graphics.on('pointerdown', clickAction)
+        }
+        graphics.alpha = 0.5;
+        return graphics;
     }
-    graphics.alpha = 0.5;
-    return graphics;
+
+    static startSpectrum() {
+        Spectrum.pixiSetup();
+        Spectrum.createColorPickDropdown();
+    }
+
 }
-
-function onHoverOver()
-{
-    this.alpha = 1;
-}
-
-function onHoverOff()
-{
-    this.alpha = 0.5;
-}
-
-
-function startSpectrum() {
-    pixiSetup();
-    createColorPickDropdown();
-}
-

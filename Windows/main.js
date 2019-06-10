1f3ee88f58b45ebab53f68d04dbee438730dc09b
Anna Thomas <47217595+AnnaKThomas@users.noreply.github.com>
2019-06-10 16:44:03 -0400

    multi-image view

--- Windows/main.js
+++ Windows/main.js
@@ -16,18 +16,19 @@ function startWindows(){
 
   // Buttons for each activity
 
-  var grainboundary_btn = new RectButton(80, 100, 40, 315, 'Jack Multi-Block Analysis', "Multiblock.jpg");
-  var lineintegral_btn = new RectButton(80, 170, 40, 315, 'Travis Line-Integral Analysis', "LineIntegral.jpg");
-  var lowmag_btn = new RectButton(80, 240, 40, 315, 'Eddie Low Magnification Imaging', "LowMag.jpg");
-  var multispec1_btn = new RectButton(80, 310, 40, 315, 'Desai Multispectrum Interface', "Multispectrum.jpg");;
+  var buttonContainer = new PIXI.Container();
 
-  var sources = ["Multiblock.jpg", "LineIntegral.jpg", "LowMag.jpg", "Multispectrum.jpg"];
+  var lowmag_btn = new RectButton(80, 100, 40, 315, 'Eddie Low Magnification Imaging', "LowMag.jpg");
+  var multispec_btn = new RectButton(80, 170, 40, 315, 'Desai Multispectrum Interface', "Multispectrum.jpg");
+  var multiblock_btn = new RectButton(80, 240, 40, 315, 'Jack Multi-Block Analysis', "Multiblock.jpg");
+  var lineintegral_btn = new RectButton(80, 310, 40, 315, 'Travis Line-Integral Analysis', "LineIntegral.jpg");
+
+  app.stage.addChild(buttonContainer);
 
 
   //Modal (popup window)
 
   var modal = document.getElementById("myModal");
-  var images = document.getElementsByClassName("myImages");
   var modalImg = document.getElementById("img01");
 
   // Get the <span> element that closes the modal
@@ -35,7 +36,10 @@ function startWindows(){
 }
 
 function RectButton(x_position, y_position, height, width, text, source){
+
+  //instantiate PIXI JS Graphics library
   var graphics = new PIXI.Graphics();
+
   graphics.beginFill(0xFFFFFF, 1); // Color and opacity
   graphics.lineStyle(2, 0x414141, 1);
   graphics.drawRect(x_position, y_position, width, height);
@@ -45,12 +49,14 @@ function RectButton(x_position, y_position, height, width, text, source){
   .on('mouseout', onHoverOff)
   .on('pointerdown', onSelect);
   graphics.alpha = 0.7;
-  app.stage.addChild(graphics);
+  graphics.source = source;
+
+  buttonContainer.addChild(graphics);
 
   const label = new PIXI.Text(text, {fontFamily: "Georgia", fontSize: 18, fill: 0x2F4F4F});
   label.position.x = x_position + 18;
   label.position.y = y_position + 10;
-  app.stage.addChild(label);
+  buttonContainer.addChild(label);
 }
 
 function onHoverOver(event){
@@ -66,7 +72,7 @@ function onHoverOff(event){
 function onSelect(event) {
   this.data = event.data;
   modal.style.display = "block";
-  modalImg.src = sources[0];
+  modalImg.src = this.source;
 }
 
 span.onclick = function() { 

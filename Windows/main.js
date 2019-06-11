70d1bc54e13318ce487fb3d246ea52cb51c781e1
Anna Thomas <47217595+AnnaKThomas@users.noreply.github.com>
2019-06-11 15:19:09 -0400

    Add files via upload

--- Windows/main.js
+++ Windows/main.js
@@ -1,38 +1,54 @@
-function startWindows(){
-
-  // background image/color
-
-  const app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    antialias: true,
-    transparent: false,
-    resolution: devicePixelRatio,
-    autoResize: true,
-    backgroundColor: 0xC9D5DA
-  });
-  document.body.appendChild(app.view);
-
-
-  // Buttons for each activity
-
-  var buttonContainer = new PIXI.Container();
-
-  var lowmag_btn = new RectButton(80, 100, 40, 315, 'Eddie Low Magnification Imaging', "LowMag.jpg");
-  var multispec_btn = new RectButton(80, 170, 40, 315, 'Desai Multispectrum Interface', "Multispectrum.jpg");
-  var multiblock_btn = new RectButton(80, 240, 40, 315, 'Jack Multi-Block Analysis', "Multiblock.jpg");
-  var lineintegral_btn = new RectButton(80, 310, 40, 315, 'Travis Line-Integral Analysis', "LineIntegral.jpg");
-
-  app.stage.addChild(buttonContainer);
-
-
-  //Modal (popup window)
+  
+  //MODAL (popup window)
 
   var modal = document.getElementById("myModal");
   var modalImg = document.getElementById("img01");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
+
+
+  //TEXT
+
+  var style = {fontFamily: 'Georgia', fontSize: 22, fill: 0xD40902};
+
+
+
+function startWindows(){
+
+  // background
+
+  var bgSprite = new PIXI.Sprite.fromImage('Images/sinteredMetal.png', true);
+  app.stage.addChild(bgSprite);
+
+
+
+  // Buttons for each activity
+
+  var lowmag_btn = new RectButton(80, 100, 40, 330, 'Eddie Low Magnification Imaging', "Images/LowMag.jpg");
+  let label1 = new PIXI.Text('Eddie Low Mag Imaging', style);
+  label1.position.x = 98;
+  label1.position.y = 108;
+  bgSprite.addChild(label1);
+
+  var multispec_btn = new RectButton(80, 170, 40, 330, 'Desai Multispectrum Interface', "Images/Multispectrum.jpg");
+  let label2 = new PIXI.Text('Desai Multispectrum Interface', style);
+  label2.position.x = 98;
+  label2.position.y = 178;
+  bgSprite.addChild(label2);
+
+  var multiblock_btn = new RectButton(80, 240, 40, 330, 'Jack Multi-Block Analysis', "Images/Multiblock.jpg");
+  let label3 = new PIXI.Text('Jack Multi-Block Analysi', style);
+  label3.position.x = 98;
+  label3.position.y = 248;
+  bgSprite.addChild(label3);
+
+  var lineintegral_btn = new RectButton(80, 310, 40, 330, 'Travis Line-Integral Analysis', "Images/LineIntegral.jpg");
+  let label4 = new PIXI.Text('Travis Line-Integral Analysis', style);
+  label4.position.x = 98;
+  label4.position.y = 318;
+  bgSprite.addChild(label4);
+
 }
 
 function RectButton(x_position, y_position, height, width, text, source){
@@ -41,40 +57,41 @@ function RectButton(x_position, y_position, height, width, text, source){
   var graphics = new PIXI.Graphics();
 
   graphics.beginFill(0xFFFFFF, 1); // Color and opacity
-  graphics.lineStyle(2, 0x414141, 1);
+  graphics.lineStyle(2, 0x2F4F4F/*0x414141*/, 1);
   graphics.drawRect(x_position, y_position, width, height);
   graphics.endFill();
   graphics.interactive = true;
+  graphics.buttonMode = true;
   graphics.on('mouseover', onHoverOver) // When mouse hovers over the button, it calls onHoverOver() function
   .on('mouseout', onHoverOff)
   .on('pointerdown', onSelect);
-  graphics.alpha = 0.7;
+  graphics.alpha = 0.5;
   graphics.source = source;
 
-  buttonContainer.addChild(graphics);
+  //bgSprite.addChild(graphics);
 
-  const label = new PIXI.Text(text, {fontFamily: "Georgia", fontSize: 18, fill: 0x2F4F4F});
-  label.position.x = x_position + 18;
-  label.position.y = y_position + 10;
-  buttonContainer.addChild(label);
 }
 
-function onHoverOver(event){
+function onHoverOver(event)
+{
   this.data = event.data;
-  this.alpha = 1;
+  this.alpha = 0.7;
 }
 
-function onHoverOff(event){
+function onHoverOff(event)
+{
   this.data = event.data;
-  this.alpha = 0.5;
+  this.alpha = 0.3;
 }
 
-function onSelect(event) {
+function onSelect(event)
+{
   this.data = event.data;
   modal.style.display = "block";
   modalImg.src = this.source;
 }
 
-span.onclick = function() { 
+span.onclick = function()
+{ 
   modal.style.display = "none";
 }

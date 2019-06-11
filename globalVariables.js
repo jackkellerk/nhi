0e06be4a3c041266626c309d35e354f40c78237c
jackkellerk <51454091+jackkellerk@users.noreply.github.com>
2019-06-11 13:29:02 -0400

    Updated files for Multi-block
    jack kellerk

--- globalVariables.js
+++ globalVariables.js
@@ -1,3 +1,32 @@
 // Determining what activity we are on
 var currentActivity = 'Welcome';
 var activityArray = ['Welcome', 'Windows', 'Zoom', 'Multispectrum', 'Line', 'Tilting', 'Multi-block'];
+
+/* These are global variables for Multi-block */
+
+// Instantiate the PIXI JS Graphics Library (this is for the UI stuff)
+var graphics = new PIXI.Graphics();
+var dragImage = null;
+
+// These are the variables for the highlight function in Multi-block
+var initialXMousePosition;
+var initialYMousePosition;
+var boxArray = [];
+var comparisonBoxArray = [];
+var informationBoxArray = [];
+var intensity = []; // holds values for pixel intensity
+var offset = 50; // 50 pixels between comparison boxes
+var width;
+var height;
+
+// These are the variables for the annotation feature in Multi-block
+var strokeArray = [];
+var drawingArray = [];
+
+// Variables for rotation feature
+var clockwiseRotation = null;
+
+// This is for the instruction feature
+var instructionContainer = new PIXI.Container();
+
+/* End of Multi-block global variables */

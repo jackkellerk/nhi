be812c273daaf8be5c34fce02bcb410989dcd8ff
desaixie <xiedesaigg@gmail.com>
2019-06-21 00:59:21 -0400

    Update on projects page and class Hexagon

--- ProjectsPage/projects.js
+++ ProjectsPage/projects.js
@@ -2,10 +2,12 @@
 class Projects{
     
     static createWindowHexagons(){
-    
+        let h1 = new Hexagon({x: app.screen.width/2, y: app.screen.height/2}, 100);
+        // h1.graphics.lineStyle(2, 0x414141, 3);
+        h1.draw(Hexagon.getHexColor("blue"));
     }
     
     static startProjects(){
-    
+        Projects.createWindowHexagons();
     }
 }
--- hexagon.js
+++ hexagon.js
@@ -2,11 +2,23 @@
 PIXI hexagon
  */
 class Hexagon{
+    static SQRT3 = Math.sqrt(3);
+    
+    /*
+    Helper function. Gets hex representation of a color given color name in string
+     */
+    static getHexColor(colorStr) {
+        let a = document.createElement('div');
+        a.style.color = colorStr;
+        let colors = window.getComputedStyle( document.body.appendChild(a) ).color.match(/\d+/g).map(function(a){ return parseInt(a,10); });
+        document.body.removeChild(a);
+        return (colors.length >= 3) ? '0x' + (((1 << 24) + (colors[0] << 16) + (colors[1] << 8) + colors[2]).toString(16).substr(1)) : false;
+    }
+    
     /*
     center: {x, y}, a Javascript object, coordinate of center
     hwidth: half width
      */
-    static SQRT3 = MATH.sqrt(3);
     constructor(center, hwidth){
         this.center = center;
         this.x = center.x;
@@ -15,6 +27,33 @@ class Hexagon{
         this.radius = this.hwidth * 2 / Hexagon.SQRT3;
         this.width = this.hwidth * 2;
         this.height = this.radius * 2;
+        
+        // Use this container to add icon and stuff that are with this hexagon
+        this.container = new PIXI.Container();
+        this.container.x = this.x;
+        this.container.y = this.y;
+        // Add click handlers to this graphics. Rememer to set this.graphics.interactive = true;
+        this.graphics = new PIXI.Graphics();
+    }
+    
+    /*
+    Customize , i.e. lineStyle, before call this function.
+    fill: color used in Graphics.beginFill(fill)
+     */
+    draw(fill=0xFFFFFF){
+        this.graphics.beginFill(fill);
+        // this.graphics.lineStyle(2, 0x414141, 3);
+        this.graphics.drawPolygon([  // every two number represents a coordinate of a point on the path of this hexagon
+            0, 0 - this.radius,
+            0 + this.hwidth, 0 - this.radius/2,
+            0 + this.hwidth, 0 + this.radius/2,
+            0, 0 + this.radius,
+            0 - this.hwidth, 0 + this.radius/2,
+            0 - this.hwidth, 0 - this.radius/2
+        ]);
+        this.graphics.endFill();
+        this.container.addChild(this.graphics);
+        app.stage.addChild(this.container);
     }
     
     /*

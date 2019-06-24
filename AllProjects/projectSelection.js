7c4c11c519b1e12ef7290948a22adf30bdf7e26b
Anna Thomas <47217595+AnnaKThomas@users.noreply.github.com>
2019-06-24 11:14:09 -0400

    added projectSelection.js

---
+++ AllProjects/projectSelection.js
@@ -0,0 +1,21 @@
+
+
+startAllProjects()
+{
+    // background from gradient texture
+
+    const gradTexture = createGradTexture();
+
+    const sprite = new PIXI.Sprite(gradTexture);
+    sprite.width = app.screen.width;
+    sprite.height = app.screen.height;
+    app.stage.addChild(sprite);
+
+
+
+
+
+
+
+
+}

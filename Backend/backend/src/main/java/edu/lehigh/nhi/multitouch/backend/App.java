fd7fecc3aa0a3d6800b4c40989165d88457cb37c
Jack Kellerk <jjk322@lehigh.edu>
2019-06-28 13:54:41 -0400

    added enableCORS method for external requests

--- Backend/backend/src/main/java/edu/lehigh/nhi/multitouch/backend/App.java
+++ Backend/backend/src/main/java/edu/lehigh/nhi/multitouch/backend/App.java
@@ -8,6 +8,9 @@ public class App {
 
         final MySQLConnection db = MySQLConnection.getConnection();
 
+        // The first column needs to be the address we are attempting to access the information from
+        enableCORS("http://localhost:8080", "*", "*");
+
         // Get the port on which to listen for requests
         // Spark.port(getIntFromEnv("PORT", 4567));
 
@@ -24,6 +27,7 @@ public class App {
             JSONObject jsRequest = new JSONObject(request.body());
             String username = jsRequest.getString("username");
             String password = jsRequest.getString("password");
+            System.out.println(username + " , " + password);
             response.status(200);
             response.type("application/json");
             if (db.login(username, password)) {
@@ -32,6 +36,33 @@ public class App {
             }
             StructuredResponse retval = new StructuredResponse(100, "Login failed", null);
             return retval.toJson().toString();
+        });
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        Spark.options("/*", (request, response) -> {
+    
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+    
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+    
+            return "OK";
+        });
+    
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Allow-Credentials", "true");
+            //response.header("Access-Control-Request-Method", methods);
+            //response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
         });
     }
 }

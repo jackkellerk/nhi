1e0bc5074cbf6a29f63b7ad3dff4b5424b18b50b
jackkellerk <51454091+jackkellerk@users.noreply.github.com>
2019-07-01 16:10:13 -0400

    Add files via upload

--- jsToBackend.js
+++ jsToBackend.js
@@ -1,40 +1,67 @@
 // This is the javascript file I added for the connection to the backend
-var theData = {"username": "admin", "password": "password"};
-
 function loginToBackend()
 {
+    // converts the username into json
+    var username = userTextBox.text;
+    var convertToJSON = {"username": username, "password": password};
 
-    // This works for GET Requests
     $.ajax({
-        method: 'GET',
-        // data: theData,
-        url: 'http://localhost:4567',
-        dataType: 'text',
-        crossDomain: true,
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(convertToJSON),
+        url: 'http://localhost:4567/login', // change to 8080 if this doesnt work
+        dataType: 'json',
+        crossDomain: 'true',
         xhrFields: {
             withCredentials: true
         },
         success: function(data) {
-            alert(data);
+            currentActivity = "AllProjects";
         },
         error: function(xhr, status, error) {
-            alert(xhr.responseText);
+            alert("Internal Server Error: 500");
         }
     });
+}
 
-    // This works for POST Requests
-    var root = 'localhost:4567';
+function signUpBackend()
+{
+    var username = SU_userTextBox.text;
+    var email = SU_emailTextBox.text;
+    var password = SU_passwordTextBox.text;
+    var confirmPassword = SU_repasswordTextBox.text;
+    var legalName = /* SU_legalNameTextBox.text; */ "James Kellerk";
+    var institution = /* SU_institutionTextBox.text; */ "Lehigh";
+    var convertToJSON = {"username": username, "password": password, "email": email, "legalname": legalName, "institution": institution};
+
+    if(confirmPassword != password)
+    {
+        alert("Passwords do not match! Try again!");
+        return;
+    }
+
+    // Ensures the email must be an edu email address
+    if(!email.includes(".edu"))
+    {
+        alert("Not a valid email address!");
+        return;
+    }
+
     $.ajax({
         method: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify(theData),
-        url: root + '/login',
+        data: JSON.stringify(convertToJSON),
+        url: 'http://localhost:4567/signup', // change to 8080 if this doesnt work
         dataType: 'json',
-        success: function(data) {
-            alert(JSON.stringify(data));
+        crossDomain: 'true',
+        xhrFields: {
+            withCredentials: true
         },
-        error: function(error) {
-            alert("Error");
+        success: function(data) {
+            currentActivity = "AllProjects";
+        },
+        error: function(xhr, status, error) {
+            alert("Internal Server Error: 500");
         }
-    })
+    });
 }

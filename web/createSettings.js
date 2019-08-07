4881c09978a1421ae2b527121b62d65bb19df3d9
Anna Thomas <47217595+AnnaKThomas@users.noreply.github.com>
2019-08-07 14:15:23 -0400

    added "add User" functionality

--- web/createSettings.js
+++ web/createSettings.js
@@ -65,65 +65,115 @@ function createSettings(title="Settings", container, fieldCount)
 
 }
 
-function s_addField(fieldName, fieldContent, fieldNo, addIcon=false)
+function s_addField(fieldName, fieldContent, fieldNo, iconType=1)
 {
+    let addUserMode = false;
+    var editMode = false;
+    //var myBox;
+    var fieldTextbox;
+    var myPlaceholder = fieldContent;
+    var myFiller = fieldContent;
+    var textbox_x = corner_x + 240;
+    if (iconType == 3) {
+        textbox_x = corner_x + 465;
+        myPlaceholder = "Search User";
+        myFiller = "";
+    }
+
     let newField = new PIXI.Text(fieldName, {fill: "#FFFFFF", fontFamily: "Helvetica", fontSize: 18, letterSpacing: 2});        
-    newField.position.x = corner_x + 35;
-    newField.position.y = corner_y + 60 + fieldNo*40;
+        newField.position.x = corner_x + 35;
+        newField.position.y = corner_y + 60 + fieldNo*40;
     s_container.addChild(newField);
 
-    let fieldtxt = new PIXI.Text(fieldContent, {fill: "#FFFFFF", fontFamily: "Helvetica", fontSize: 16});
-        fieldtxt.x = corner_x + 250;
-        fieldtxt.y = newField.y;
-    s_container.addChild(fieldtxt);
+    if (iconType == 3) {
+        userText = new PIXI.Text(fieldContent, {fill: "#FFFFFF", fontFamily: "Helvetica", fontSize: 16});
+        userText.x = corner_x + 250;
+        userText.y = newField.y;
+        s_container.addChild(userText);
+    }
+
+    fieldTextbox = new PIXI.TextInput({
+        input: {
+            fontFamily: 'Helvetica',
+            fontSize: '12pt',
+            padding: '10px',
+            width: '220px',
+            color: '#FFFFFF'
+        }, 
+        box: s_textbox(textbox_x, newField.y-7)
+    });
+    fieldTextbox.x = textbox_x;
+    fieldTextbox.y = newField.y-7;
+    fieldTextbox.interactiveChildren = true;
+    fieldTextbox.placeholder = myPlaceholder;
+    fieldTextbox.text = myFiller;
+    s_container.addChild(fieldTextbox);
+    editMode = false;
+    //s_container.removeChild(myBox);
+    s_textbox(0, -1);
+
+    if (iconType == 3) {
+        s_container.removeChild(fieldTextbox);
+    }
 
     let editIcon = new PIXI.Sprite.from("Images/edit-icon.png");
-    if (addIcon) { editIcon = new PIXI.Sprite.from("Images/add-icon.png"); }
-        editIcon.width = 30;
-        editIcon.height = 30;
-        editIcon.x = corner_x + 465;
-        editIcon.y = newField.y - 5;
-        editIcon.buttonMode = true;
-        editIcon.interactive = true;
-        editIcon.on('mouseover', function(){ editIcon.alpha = 0.8; });
-        editIcon.on('mouseout', function(){ editIcon.alpha = 0.4; });
-        editIcon.on('pointerdown', function(){
-            s_container.removeChild(fieldtxt);
-            s_editMode(corner_x+240, newField.y-7, fieldContent);
-        });
-        editIcon.alpha = 0.4;
+    if (iconType>1) { editIcon = new PIXI.Sprite.from("Images/add-icon.png"); }
+    editIcon.width = 30;
+    editIcon.height = 30;
+    editIcon.x = corner_x + 465;
+    editIcon.y = newField.y - 5;
+    editIcon.buttonMode = true;
+    editIcon.interactive = true;
+    editIcon.on('mouseover', function(){ editIcon.alpha = 0.8; });
+    editIcon.on('mouseout', function(){ editIcon.alpha = 0.4; });
+    editIcon.on('pointerdown', function(){
+        if(iconType<3) {
+            if (!editMode) {
+                s_textbox(-1, 0);
+                fieldTextbox.select();
+                editMode = true;
+            }
+            else if (editMode) {
+                s_textbox(0, -1);
+                fieldTextbox.blur();
+                editMode = false;
+            }
+        } else if(iconType==3) {
+            if(!addUserMode) {
+                s_textbox(-1, 0);
+                editIcon.x = corner_x + 690;
+                s_container.addChild(fieldTextbox);
+                addUserMode = true;
+            } else if (addUserMode) {
+                s_textbox(0, -1);
+                s_container.removeChild(editIcon);
+                s_container.removeChild(fieldTextbox);
+                s_addField("", fieldTextbox.text+ " (request sent)", fieldNo+1, 3);
+                addUserMode = false;
+            }
+        }
+    });
+    editIcon.alpha = 0.4;
     s_container.addChild(editIcon);
 
 
 }
 
-function s_editMode(myX, myY, myText)
-{
-    let fieldTextbox = new PIXI.TextInput({
-        input: {
-            fontFamily: 'Tahoma',
-            fontSize: '11pt',
-            padding: '10px',
-            width: '220px',
-            color: '#000000'
-        }, 
-        box: s_textbox(myX, myY)
-    });
-    fieldTextbox.x = myX;
-    fieldTextbox.y = myY;
-    fieldTextbox.interactiveChildren = true;
-    fieldTextbox.placeholder = myText;
-    fieldTextbox.text = myText;
-    s_container.addChild(fieldTextbox);
-}
-
 function s_textbox(myX, myY)
-{
-    box = new PIXI.Graphics();
-    box.beginFill(0xFFFFFF);
-    box.drawRoundedRect(myX,myY, 220,34, 0);
-    //box.alpha = 0.1;
-    s_container.addChild(box);
+{   
+    if(myX<0) {
+        myBox.alpha = 1;
+        return;
+    } else if (myY<0) {
+        myBox.alpha = 0;
+        return;
+    }
+    myBox = new PIXI.Graphics();
+    myBox.beginFill(0xA0A0A0);
+    myBox.drawRoundedRect(myX,myY, 220,34, 2);
+    myBox.alpha = 0;
+    s_container.addChild(myBox);
+
 }
 
 function s_addLogoutButton()

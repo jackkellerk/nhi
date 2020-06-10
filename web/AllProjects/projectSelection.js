c745f1e66a75166326843fb1a13bf230b87d9d36
Agus-27 <agm321@lehigh.edu>
2020-06-10 12:05:46 -0400

    Cleaned maven path files/ Test ajax call to project backend

--- web/AllProjects/projectSelection.js
+++ web/AllProjects/projectSelection.js
@@ -25,8 +25,8 @@ var userSettingsResponse; // valid uses of userSettingsResponse are: userSetting
 function startAllProjects()
 {
     gatherUserSettings(); // backend call
-    // getOneProject(76);
-    // getProjects(); TODO: Find out what is happening here
+    getOneProject(100);
+    getProjects(); //TODO: Find out what is happening here
 }
 
 function createUIProjects()
